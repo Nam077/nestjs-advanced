2ad97ad51222a52d4484d6d7c4237d92888ebc98
Nam077 <nam077.me@gmail.com>
2024-09-20 20:41:08 +0700

    fix: configure commitlint and husky for Git workflow

--- src/modules/cache/cache.module.ts
+++ src/modules/cache/cache.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
-import { RedisModule } from '@nestjs-modules/ioredis';
 import { ConfigModule } from '@nestjs/config';
+
+import { RedisModule } from '@nestjs-modules/ioredis';
+
 import { CacheConfigService } from './cache-config/cache-config.service';
 
+/**
+ *
+ */
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
